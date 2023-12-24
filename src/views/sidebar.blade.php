320ffb9d28778eecae22a28fede03ee34b4f3745
Achyut Neupane <achyutkneupane@gmail.com>
2023-12-24 10:28:15 +0545

    feat: test-route check on links in blade file

--- src/views/sidebar.blade.php
+++ src/views/sidebar.blade.php
@@ -23,112 +23,119 @@
     <hr/>
     <ul class="nav nav-pills flex-column mb-auto gap-0 flex-nowrap">
         @foreach(config('dashboard.sidebar') as $item)
-            @php
-                $subRoutes = [];
-                $noMenuRoutes = [];
-                if(!array_key_exists('route', $item)) {
+            @if(!array_key_exists('test-route', $item) || (array_key_exists('test-route', $item) && config('dashboard.enableTestRoutes') === true))
+                @php
                     $subRoutes = [];
-                    foreach($item['submenu'] as $subItem) {
-                        $subRoutes[] = config('dashboard.prefix').$subItem['route'];
-                    }
-                }
-                $noMenuRoutes = [];
-                if(array_key_exists('noMenuRoutes', $item)) {
-                    foreach($item['noMenuRoutes'] as $noMenuRoute) {
-                        $noMenuRoutes[] = config('dashboard.prefix').$noMenuRoute;
-                    }
-                }
-                $slug = str_replace(' ', '-', $item['title']);
-
-                $guards = [];
-                if(array_key_exists('guard', $item)) {
-                    $guards[] = $item['guard'];
-                }
-                if(array_key_exists('submenu', $item)) {
-                    foreach($item['submenu'] as $subItem) {
-                        if(array_key_exists('guard', $subItem)) {
-                            $guards[] = $subItem['guard'];
+                    $noMenuRoutes = [];
+                    if(!array_key_exists('route', $item)) {
+                        $subRoutes = [];
+                        foreach($item['submenu'] as $subItem) {
+                            $subRoutes[] = config('dashboard.prefix').$subItem['route'];
                         }
                     }
-                }
-            @endphp
-            @if((array_key_exists('guard', $item) && auth()->user()->canAny($guards)) || !array_key_exists('guard', $item))
-                <li class="nav-item py-1">
-                    @if(!array_key_exists('route', $item))
-                        <a class="nav-link @if(in_array(Route::currentRouteName(), $subRoutes) || in_array(Route::currentRouteName(), $noMenuRoutes)) active @endif p-1 px-3 pe-auto user-select-none"
-                           aria-current="page" data-bs-toggle="collapse" href="#{{ $slug }}Collapse" role="button"
-                           aria-expanded="@if(in_array(Route::currentRouteName(), $subRoutes) || in_array(Route::currentRouteName(), $noMenuRoutes)) true @else false @endif"
-                           aria-controls="#{{ $slug }}Collapse">
-                            @else
-                                <a href="{{ route(config('dashboard.prefix').$item['route']) }}"
-                                   class="nav-link @if(request()->routeIs(config('dashboard.prefix').$item['route'])) active @endif"
-                                   aria-current="page">
-                                    @endif
+                    $noMenuRoutes = [];
+                    if(array_key_exists('noMenuRoutes', $item)) {
+                        foreach($item['noMenuRoutes'] as $noMenuRoute) {
+                            $noMenuRoutes[] = config('dashboard.prefix').$noMenuRoute;
+                        }
+                    }
+                    $slug = str_replace(' ', '-', $item['title']);
 
-                                    <div class="d-flex align-items-center gap-2">
-                                        @if(config('dashboard.icons') == 'svg')
-                                            @if(array_key_exists('route', $item))
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 600"
-                                                     fill="@if(request()->routeIs(config('dashboard.prefix').$item['route'])) #252834 @else #ffffff @endif"
-                                                     style="width: 30px; height: 30px;">
-                                                    @else
-                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 600"
-                                                             fill="@if(in_array(Route::currentRouteName(), $subRoutes) || in_array(Route::currentRouteName(), $noMenuRoutes)) #252834 @else white @endif"
-                                                             style="width: 25px; height: 25px; padding:1px;" class="">
-                                                            @endif
-
-                                                            <path d="{{ $item['icon'] }}"/>
-
-                                                            @if(array_key_exists('route', $item))
-                                                        </svg>
-                                                        @else
-                                                </svg>
-                                            @endif
-                                        @elseif(config('dashboard.icons') == 'font-awesome')
-                                            <i class="fas fa-{{ $item['icon'] }} fa-fw"></i>
-                                        @endif
-                                        <span class="routeText">{{ $item['title'] }}</span>
-                                    </div>
-
-                                    @if(array_key_exists('route', $item))
-                                </a>
+                    $guards = [];
+                    if(array_key_exists('guard', $item)) {
+                        $guards[] = $item['guard'];
+                    }
+                    if(array_key_exists('submenu', $item)) {
+                        foreach($item['submenu'] as $subItem) {
+                            if(array_key_exists('guard', $subItem)) {
+                                $guards[] = $subItem['guard'];
+                            }
+                        }
+                    }
+                @endphp
+                @if((array_key_exists('guard', $item) && auth()->user()->canAny($guards)) || !array_key_exists('guard', $item))
+                    <li class="nav-item py-1">
+                        @if(!array_key_exists('route', $item))
+                            <a class="nav-link @if(in_array(Route::currentRouteName(), $subRoutes) || in_array(Route::currentRouteName(), $noMenuRoutes)) active @endif p-1 px-3 pe-auto user-select-none"
+                               aria-current="page" data-bs-toggle="collapse" href="#{{ $slug }}Collapse" role="button"
+                               aria-expanded="@if(in_array(Route::currentRouteName(), $subRoutes) || in_array(Route::currentRouteName(), $noMenuRoutes)) true @else false @endif"
+                               aria-controls="#{{ $slug }}Collapse">
                                 @else
-                        </a>
-                    @endif
+                                    <a href="{{ route(config('dashboard.prefix').$item['route']) }}"
+                                       class="nav-link @if(request()->routeIs(config('dashboard.prefix').$item['route'])) active @endif"
+                                       aria-current="page">
+                                        @endif
 
-                    @if(array_key_exists('submenu', $item))
-                        <div
-                                class="collapse @if(in_array(Route::currentRouteName(), $subRoutes) || in_array(Route::currentRouteName(), $noMenuRoutes)) show @endif"
-                                id="{{ $slug }}Collapse">
-                            <ul class="d-flex flex-column align-items-end mt-2 pb-0 nav nav-pills">
-                                @foreach($item['submenu'] as $index => $subItem)
-                                    @if((array_key_exists('guard', $subItem) && auth()->user()->can($subItem['guard'])) || !array_key_exists('guard', $subItem))
-                                        <li class="nav-item" style="width: 90%;">
-                                            <a href="{{ route(config('dashboard.prefix').$subItem['route']) }}"
-                                               class="nav-link @if(request()->routeIs(config('dashboard.prefix').$subItem['route'])) active @endif p-1 px-2"
-                                               aria-current="page">
-                                                <div class="d-flex align-items-center gap-2">
-                                                    @if(config('dashboard.icons') == 'svg')
-                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 750 750"
-                                                             fill="@if(request()->routeIs($subItem['route'])) #252834 @else #ffffff @endif"
-                                                             style="width: 30px; height: 30px;">
-                                                            <path d="{{ $subItem['icon'] }}"/>
-                                                        </svg>
-                                                    @elseif(config('dashboard.icons') == 'font-awesome')
-                                                        <i class="fas fa-{{ $subItem['icon'] }} fa-fw"></i>
-                                                    @endif
-                                                    <span class="routeText">{{ $subItem['title'] }}</span>
-                                                </div>
-                                            </a>
-                                        </li>
-                                    @endif
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                </li>
-                <hr class="my-1"/>
-            @endcan
+                                        <div class="d-flex align-items-center gap-2">
+                                            @if(config('dashboard.icons') == 'svg')
+                                                @if(array_key_exists('route', $item))
+                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 600"
+                                                         fill="@if(request()->routeIs(config('dashboard.prefix').$item['route'])) #252834 @else #ffffff @endif"
+                                                         style="width: 30px; height: 30px;">
+                                                        @else
+                                                            <svg xmlns="http://www.w3.org/2000/svg"
+                                                                 viewBox="0 0 600 600"
+                                                                 fill="@if(in_array(Route::currentRouteName(), $subRoutes) || in_array(Route::currentRouteName(), $noMenuRoutes)) #252834 @else white @endif"
+                                                                 style="width: 25px; height: 25px; padding:1px;"
+                                                                 class="">
+                                                                @endif
+
+                                                                <path d="{{ $item['icon'] }}"/>
+
+                                                                @if(array_key_exists('route', $item))
+                                                            </svg>
+                                                            @else
+                                                    </svg>
+                                                @endif
+                                            @elseif(config('dashboard.icons') == 'font-awesome')
+                                                <i class="fas fa-{{ $item['icon'] }} fa-fw"></i>
+                                            @endif
+                                            <span class="routeText">{{ $item['title'] }}</span>
+                                        </div>
+
+                                        @if(array_key_exists('route', $item))
+                                    </a>
+                                    @else
+                            </a>
+                        @endif
+
+                        @if(array_key_exists('submenu', $item))
+                            <div
+                                    class="collapse @if(in_array(Route::currentRouteName(), $subRoutes) || in_array(Route::currentRouteName(), $noMenuRoutes)) show @endif"
+                                    id="{{ $slug }}Collapse">
+                                <ul class="d-flex flex-column align-items-end mt-2 pb-0 nav nav-pills">
+                                    @foreach($item['submenu'] as $index => $subItem)
+                                        @if(!array_key_exists('test-route', $subItem) || (array_key_exists('test-route', $subItem) && config('dashboard.enableTestRoutes') === true))
+                                            @if((array_key_exists('guard', $subItem) && auth()->user()->can($subItem['guard'])) || !array_key_exists('guard', $subItem))
+                                                <li class="nav-item" style="width: 90%;">
+                                                    <a href="{{ route(config('dashboard.prefix').$subItem['route']) }}"
+                                                       class="nav-link @if(request()->routeIs(config('dashboard.prefix').$subItem['route'])) active @endif p-1 px-2"
+                                                       aria-current="page">
+                                                        <div class="d-flex align-items-center gap-2">
+                                                            @if(config('dashboard.icons') == 'svg')
+                                                                <svg xmlns="http://www.w3.org/2000/svg"
+                                                                     viewBox="0 0 750 750"
+                                                                     fill="@if(request()->routeIs($subItem['route'])) #252834 @else #ffffff @endif"
+                                                                     style="width: 30px; height: 30px;">
+                                                                    <path d="{{ $subItem['icon'] }}"/>
+                                                                </svg>
+                                                            @elseif(config('dashboard.icons') == 'font-awesome')
+                                                                <i class="fas fa-{{ $subItem['icon'] }} fa-fw"></i>
+                                                            @endif
+                                                            <span class="routeText">{{ $subItem['title'] }}</span>
+                                                        </div>
+                                                    </a>
+                                                </li>
+                                            @endif
+                                        @endif
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                    </li>
+                    <hr class="my-1"/>
+                @endcan
+            @endif
         @endforeach
     </ul>
     <ul class="nav nav-pills flex-column mb-0">
